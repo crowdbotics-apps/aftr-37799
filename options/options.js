91df4c543d9a53809a54d97d1a1e57d845637542
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2022-12-09 13:55:07 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "aftr_37799",
   url: "https://aftr_37799.botics.co",
   api: "https://aftr_37799.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
